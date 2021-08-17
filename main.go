fd47dd4fed27242a3e83119334ee0bd9efd59cac
Daniel Schreiber <daniel.schreiber11@gmail.com>
2021-08-17 12:13:31 -0500

    feat: added simple json based database

--- main.go
+++ main.go
@@ -4,22 +4,36 @@ import (
 	"log"
 	"encoding/json"
 	"os"
+    scribble "github.com/nanobox-io/golang-scribble"
 )
 
 func main() {
-	config := ParseConfig()
-	err := StartServer(config.Server)
+    config := ParseConfig()
+    db, err := scribble.New(config.Database.Filepath, nil)
+    if err != nil {
+        panic(err)
+    }
+    if err := db.Write("songs", "test", nil); err != nil {
+        log.Fatal(err)
+    }
+	err = StartServer(config.Server)
     log.Fatal(err)
 }
 
 type Config struct {
 	Server ServerInfo
+    Database DatabaseInfo
 }
+
 type ServerInfo struct {
 	Host, Port string
 }
 func (server ServerInfo) GetURL() string {
 	return server.Host + ":" + server.Port
+}
+
+type DatabaseInfo struct {
+    Filepath string
 }
 
 func ParseConfig() Config {
