334ead506d1512a802116d28ff304f1f2f000a88
Daniel Schreiber <daniel.schreiber11@gmail.com>
2021-08-18 17:57:07 -0500

    feat: installable as pwa

---
+++ static/service-worker.js
@@ -0,0 +1,25 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+self.addEventListener('fetch', function(event) {
+event.respondWith(async function() {
+   try{
+     var res = await fetch(event.request);
+     var cache = await caches.open('cache');
+     cache.put(event.request.url, res.clone());
+     return res;
+   }
+   catch(error){
+     return caches.match(event.request);
+    }
+  }());
+});
